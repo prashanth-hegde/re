e290aa86fe147b3d31022e5a5340554186ab2728
Prashanth.Hegde <Prashanth.Hegde@target.com>
2022-09-01 11:49:11 -0500

    fixing whitespaces and converting spaces to tabs

--- matcher.v
+++ matcher.v
@@ -1,69 +1,69 @@
 module main
 
 pub struct RegexOpts {
-  ignore_case       bool
-  standard_expr     bool
+	ignore_case			 bool
+	standard_expr		 bool
 }
 
 // recursively add states for epsilons
 [inline]
 fn add_state(s State, mut state_set []State) {
-  log.debug('adding state $s')
-  state_names := state_set.map(it.name)
-  if s.name !in state_names {
-    state_set << s
-    for eps in s.epsilon {
-      add_state(eps, mut state_set)
-    }
-  }
+	log.debug('adding state $s')
+	state_names := state_set.map(it.name)
+	if s.name !in state_names {
+		state_set << s
+		for eps in s.epsilon {
+			add_state(eps, mut state_set)
+		}
+	}
 }
 
 [inline]
 fn can_transition(state &State, ch rune) ?&State {
-  mut res := false
-  for tr in state.transitions {
-    log.debug("evaluating state transition $tr.token against $ch")
-    res = match tr.token.symbol {
-      .char           {
-        log.debug("char $ch being matched ${tr.token.char == ch.str()}")
-        tr.token.char == ch.str() }
-      .dot            { true }
-      else            { false }
-    }
-    if res {
-      return tr.state
-    }
-  }
-  return error("no matching state found for $state.name")
+	mut res := false
+	for tr in state.transitions {
+		log.debug("evaluating state transition $tr.token against $ch")
+		res = match tr.token.symbol {
+			.char					 {
+				log.debug("char $ch being matched ${tr.token.char == ch.str()}")
+				tr.token.char == ch.str() }
+			.dot						{ true }
+			else						{ false }
+		}
+		if res {
+			return tr.state
+		}
+	}
+	return error("no matching state found for $state.name")
 }
 
 [direct_array_access]
 fn (re Re) match_all(text string) bool {
-  // todo: externalize this processing logic
-  // todo: modify the logic to process indexes rather than text itself
-  log.debug('$re.transit')
-  mut curr_states := []State{}
-  add_state(re.transit.start, mut curr_states)
-  for ch in text.runes() {
-    mut next_states := []State{}
-    for state in curr_states {
-      next_state := can_transition(state, ch) or { continue }
-      add_state(next_state, mut next_states)
-    }
-    curr_states = next_states.clone()
-    log.debug("next_states: $curr_states")
-    if curr_states.len == 0 {
-      break
-    }
-  }
+	// todo: externalize this processing logic
+	// todo: modify the logic to process indexes rather than text itself
+	log.debug('$re.transit')
+	mut curr_states := []State{}
+	add_state(re.transit.start, mut curr_states)
+	for ch in text.runes() {
+		mut next_states := []State{}
+		for state in curr_states {
+			next_state := can_transition(state, ch) or { continue }
+			add_state(next_state, mut next_states)
+		}
+		curr_states = next_states.clone()
+		log.debug("next_states: $curr_states")
+		if curr_states.len == 0 {
+			break
+		}
+	}
 
-  for s in curr_states {
-    if s.is_end {
-      log.debug("evaluating end for $s.name")
-      return true
-    }
-  }
-  return false
+	for s in curr_states {
+		if s.is_end {
+			log.debug("evaluating end for $s.name")
+			return true
+		}
+	}
+	return false
 }
 
 /******************************************************************************
@@ -72,28 +72,28 @@ fn (re Re) match_all(text string) bool {
 *
 ******************************************************************************/
 pub struct Re {
-  transit           Transition
-  opts              RegexOpts
+	transit					 Transition
+	opts							RegexOpts
 }
 
 pub fn compile_opt(pattern string, opts RegexOpts) ?Re {
-  return Re{transit: build_nfa(pattern)?, opts: opts}
+	return Re{transit: build_nfa(pattern)?, opts: opts}
 }
 
 pub fn compile(pattern string) ?Re {
-  return Re{transit: build_nfa(pattern)?, opts:RegexOpts{}}
+	return Re{transit: build_nfa(pattern)?, opts:RegexOpts{}}
 }
 
 pub fn match_all(pattern string, txt string) ?bool {
-  mut compiled := false
-  re := fn [pattern, mut compiled] () ?Re {
-    mut re_ := Re{}
-    if !compiled {
-      re_ = compile(pattern)?
-      compiled = true
-    }
-    return re_
-  }
-  return re()?.match_all(txt)
+	mut compiled := false
+	re := fn [pattern, mut compiled] () ?Re {
+		mut re_ := Re{}
+		if !compiled {
+			re_ = compile(pattern)?
+			compiled = true
+		}
+		return re_
+	}
+	return re()?.match_all(txt)
 }
 
--- tokens.v
+++ tokens.v
@@ -10,14 +10,14 @@ enum Symbol {
 	concat								// \x08
 	plus									// +
 	qmark 								// ?
-	dot									  // .
+	dot										// .
 	char									// normal character
-  word                  // \w
-  nonword               // \W
-  digit                 // \d
-  nondigit              // \D
-  space                 // \s
-  nonspace              // \S
+	word									// \w
+	nonword								// \W
+	digit									// \d
+	nondigit							// \D
+	space									// \s
+	nonspace							// \S
 }
 const log = Log{level: .debug}
 const concat = `\x08`
@@ -34,22 +34,22 @@ const symbol_map = {
 }
 
 const switches = {
-  `.`                   : Symbol.dot
-  `w`                   : .word
-  `W`                   : .nonword
-  `d`                   : .digit
-  `D`                   : .nondigit
-  `s`                   : .space
-  `S`                   : .nonspace
+	`.`									 : Symbol.dot
+	`w`									 : .word
+	`W`									 : .nonword
+	`d`									 : .digit
+	`D`									 : .nondigit
+	`s`									 : .space
+	`S`									 : .nonspace
 }
 
 /*
 	Token maintains a simple mapping of character with any special meaning
-  For instance, a * is interpreted literally if it is preceeded with a backslash
-  But * has a special meaning if it is not preceeded by a backslash
+	For instance, a * is interpreted literally if it is preceeded with a backslash
+	But * has a special meaning if it is not preceeded by a backslash
 */
 struct Token {
-	char 									string 			  [required]
+	char 									string 				[required]
 	symbol 								Symbol 				[required]
 }
 
@@ -65,138 +65,137 @@ fn (token Token) str() string {
 
 struct Parser {
 	pattern 							string 				[required]
-  runes                 []rune
+	runes								 []rune
 	mut:
 	position 							int
 	tokens 								[]Token
-  raw_tokens            []Token
-  curr_token            Token
-  curr_group            int           = 1
+	raw_tokens						[]Token
+	curr_token						Token
+	curr_group						int					 = 1
 }
 
 fn (parser Parser) string() string {
-  return "position=$parser.position | curr=$parser.curr_token.symbol | lookahead=$parser.lookahead().symbol"
+	return "position=$parser.position | curr=$parser.curr_token.symbol | lookahead=$parser.lookahead().symbol"
 }
 
 fn (mut parser Parser) get_token(escaped bool) Token {
-  if parser.position >= parser.runes.len {
-    return end_token
-  }
+	if parser.position >= parser.runes.len {
+		return end_token
+	}
 
-  chr := parser.runes[parser.position]
-  token := if escaped && chr == `.` {
-    parser.position++
-    Token{'.', .char}
-  } else if !escaped && chr == `.` {
-    parser.position++
-    Token{dot, .dot}
-  } else if escaped  && chr in switches {
-    parser.position++
-    sw := switches[chr] or { Symbol.char }
-    Token{chr.str(), sw}
-  } else if !escaped && chr == `\\` {
-    parser.position++
-    parser.get_token(true)
-  } else if !escaped && chr in symbol_map {
-    parser.position++
-    sym := symbol_map[chr] or { Symbol.char }
-    Token{chr.str(), sym}
-  } else {
-    parser.position++
-    Token{chr.str(), .char}
-  }
-  return token
+	chr := parser.runes[parser.position]
+	return if escaped && chr == `.` {
+		parser.position++
+		Token{'.', .char}
+	} else if !escaped && chr == `.` {
+		parser.position++
+		Token{dot, .dot}
+	} else if escaped	&& chr in switches {
+		parser.position++
+		sw := switches[chr] or { Symbol.char }
+		Token{chr.str(), sw}
+	} else if !escaped && chr == `\\` {
+		parser.position++
+		parser.get_token(true)
+	} else if !escaped && chr in symbol_map {
+		parser.position++
+		sym := symbol_map[chr] or { Symbol.char }
+		Token{chr.str(), sym}
+	} else {
+		parser.position++
+		Token{chr.str(), .char}
+	}
 }
 
 fn (parser Parser) lookahead() Token {
-  pos := parser.position
-  return if pos >= parser.raw_tokens.len {
-    end_token
-  } else {
-    parser.raw_tokens[pos]
-  }
+	pos := parser.position
+	return if pos >= parser.raw_tokens.len {
+		end_token
+	} else {
+		parser.raw_tokens[pos]
+	}
 }
 
 fn (mut parser Parser) next_token() Token {
-  return if parser.position >= parser.raw_tokens.len {
-    end_token
-  } else {
-    tok := parser.raw_tokens[parser.position]
-    parser.position++
-    tok
-  }
+	return if parser.position >= parser.raw_tokens.len {
+		end_token
+	} else {
+		tok := parser.raw_tokens[parser.position]
+		parser.position++
+		tok
+	}
 }
 
 /**
-  Now since the foundationals have been established it is time to
-  look at the precedence of operation:
-    concat > opt > factor (*,+,?) > group ('(', ')')
-  So we define all these operations, with recursive dependencies
+	Now since the foundationals have been established it is time to
+	look at the precedence of operation:
+		concat > opt > factor (*,+,?) > group ('(', ')')
+	So we define all these operations, with recursive dependencies
 */
 
 fn (mut parser Parser) parse() []Token {
-  // init the lookahead to begin with
-  parser.opt()
-  return parser.tokens
+	// init the lookahead to begin with
+	parser.opt()
+	return parser.tokens
 }
 
 fn (mut p Parser) opt() {
-  p.concat()
-  if p.lookahead().symbol == .opt {
-    tok := p.lookahead()
-    p.next_token()
-    p.opt()
-    p.tokens << tok
-  }
+	p.concat()
+	if p.lookahead().symbol == .opt {
+		tok := p.lookahead()
+		p.next_token()
+		p.opt()
+		p.tokens << tok
+	}
 }
 
 fn (mut p Parser) concat() {
-  p.factor()
-  if p.lookahead().symbol !in [.group_end, .opt, .end] {
-    p.concat()
-    p.tokens << Token{concat.str(), .concat}
-  } else if p.lookahead().symbol == .group_end {
-    p.tokens << Token{'0', .group_end}
-  }
+	p.factor()
+	if p.lookahead().symbol !in [.group_end, .opt, .end] {
+		p.concat()
+		p.tokens << Token{concat.str(), .concat}
+	} else if p.lookahead().symbol == .group_end {
+		p.tokens << Token{'0', .group_end}
+	}
 }
 
 fn (mut p Parser) factor() {
-  p.primary()
-  if p.lookahead().symbol in [.star, .plus, .qmark] {
-    p.tokens << p.lookahead()
-    p.next_token()
-  }
+	p.primary()
+	if p.lookahead().symbol in [.star, .plus, .qmark] {
+		p.tokens << p.lookahead()
+		p.next_token()
+	}
 }
 
 fn (mut p Parser) primary() {
-  if p.lookahead().symbol == .group_start {
-    p.tokens << Token{'${p.curr_group++}', .group_start}
-    p.next_token()
-    p.opt()
-    p.next_token()
-  } else {
-    p.tokens << p.lookahead()
-    p.next_token()
-  }
+	if p.lookahead().symbol == .group_start {
+		p.tokens << Token{'${p.curr_group++}', .group_start}
+		p.next_token()
+		p.opt()
+		p.next_token()
+	} else {
+		p.tokens << p.lookahead()
+		p.next_token()
+	}
 }
 
 fn parse(expr string) []Token {
-  mut parser := Parser{pattern:expr, runes:expr.runes()}
-  mut tok := parser.get_token(false)
-  for tok.symbol != .end {
-    parser.raw_tokens << tok
-    tok = parser.get_token(false)
-  }
-  parser.position = 0
-  return parser.parse()
+	mut parser := Parser{pattern:expr, runes:expr.runes()}
+	mut tok := parser.get_token(false)
+	for tok.symbol != .end {
+		parser.raw_tokens << tok
+		tok = parser.get_token(false)
+	}
+	parser.position = 0
+	return parser.parse()
 }
 
 fn main() {
-  //expr := r'ab((ppp)cd+)+(e*)'
-  //expr1 := r'abcd'
-  //text := r'abcddddcddcdee'
-  //toks := parse(expr1)
-  //println("toks = $toks")
-  matc := match_all('ab.d', 'abcd') ?
-  println(matc)
+	//expr := r'ab((ppp)cd+)+(e*)'
+	//expr1 := r'abcd'
+	//text := r'abcddddcddcdee'
+	//toks := parse(expr1)
+	//println("toks = $toks")
+	matc := match_all('ab.d', 'abcd') ?
+	println(matc)
 }
